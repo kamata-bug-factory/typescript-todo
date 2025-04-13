6cf215819348ff54ac6dbf54b9e091140e5abf85
kamata-bug-factory <kazukazuki517@gmail.com>
2025-04-14 01:03:57 +0900

    fix: TodoUtilを直す

--- todo-app/src/utils/TodoUtil.ts
+++ todo-app/src/utils/TodoUtil.ts
@@ -10,7 +10,7 @@ export class TodoUtil {
       if (!showingDone && item.done) {
         return false;
       }
-      return item.text;
+      return item.text.includes(keyword);
     });
   };
 }
